83f41ba1afb9ece35eeec23c59e1a7646820a15f
ArthurSauerr <germanooarthuur@gmail.com>
2023-09-25 18:54:55 -0300

    Implementado CRUD de Usuario

---
+++ src/main/java/com/alugarcarros/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.alugarcarros.controller;
+
+import com.alugarcarros.exceptions.CampoInvalidoException;
+import com.alugarcarros.model.entidade.UsuarioVO;
+import com.alugarcarros.model.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/usuarios")
+@CrossOrigin(origins = {"http://localhost:4200","http://localhost:5500"}, maxAge = 3600)
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<UsuarioVO> listarTodos(){
+        List<UsuarioVO> usuarios = usuarioService.listarTodos();
+        return usuarios;
+    }
+
+    @PostMapping
+    public UsuarioVO cadastrar(@RequestBody UsuarioVO novoUsuario) throws CampoInvalidoException{
+        return usuarioService.inserir(novoUsuario);
+    }
+
+    @PutMapping
+    public boolean atualizar(@RequestBody UsuarioVO usuarioAtualizar) throws CampoInvalidoException{
+        return usuarioService.atualizar(usuarioAtualizar) != null;
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean excluir(@PathVariable Integer id){
+        return usuarioService.excluir(id);
+    }
+
+}
---
+++ src/main/java/com/alugarcarros/model/service/UsuarioService.java
@@ -0,0 +1,53 @@
+package com.alugarcarros.model.service;
+
+import com.alugarcarros.exceptions.CampoInvalidoException;
+import com.alugarcarros.model.entidade.CarroVO;
+import com.alugarcarros.model.entidade.UsuarioVO;
+import com.alugarcarros.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<UsuarioVO> listarTodos(){ return usuarioRepository.findAll(); }
+
+    public UsuarioVO inserir(UsuarioVO novoUsuario) throws CampoInvalidoException{
+        return usuarioRepository.save(novoUsuario);
+    }
+
+    public UsuarioVO atualizar(UsuarioVO usuarioAtualizar) throws CampoInvalidoException{
+        validarCamposObrigatorios(usuarioAtualizar);
+        return usuarioRepository.save(usuarioAtualizar);
+    }
+
+    public boolean excluir(Integer id){
+        usuarioRepository.deleteById(id.longValue());
+        return true;
+    }
+
+    private void validarCamposObrigatorios(UsuarioVO usuario) throws CampoInvalidoException{
+        String mensagemValidacao = "";
+        mensagemValidacao += validarCampoString(usuario.getNome(), "nome");
+        mensagemValidacao += validarCampoString(usuario.getCpf(), "cpf");
+        mensagemValidacao += validarCampoString(usuario.getCnh(), "cnh");
+
+        if(!mensagemValidacao.isEmpty()){
+            throw new CampoInvalidoException(mensagemValidacao);
+        }
+
+    }
+
+    private String validarCampoString(String valorCampo, String nomeCampo) {
+        if(valorCampo == null || valorCampo.trim().isEmpty()){
+            return "Informe o " +nomeCampo + " \n";
+        }
+        return "";
+    }
+
+}
