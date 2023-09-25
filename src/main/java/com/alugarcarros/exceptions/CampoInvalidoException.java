cea846d5fc5818aedbb1f6d9f368f570e81151d6
Arthur Sauer <germanooarthuur@gmail.com>
2023-09-25 10:50:34 -0300

    Implementado exception básico

---
+++ src/main/java/com/alugarcarros/exceptions/CampoInvalidoException.java
@@ -0,0 +1,8 @@
+package com.alugarcarros.exceptions;
+
+public class CampoInvalidoException extends Exception{
+
+    public CampoInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
