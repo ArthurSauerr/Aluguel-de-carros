d421960b1b83bf18d35ad8076481b68910344046
Arthur Sauer <germanooarthuur@gmail.com>
2023-09-25 10:50:16 -0300

    Criação do projeto

---
+++ src/main/java/com/alugarcarros/AlugarcarrosApplication.java
@@ -0,0 +1,13 @@
+package com.alugarcarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugarcarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugarcarrosApplication.class, args);
+	}
+
+}
